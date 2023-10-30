2521d4ebd643ae5bb5d9891931e750eef30f577a
mrozek <krystof.mro@gmail.com>
2023-10-30 18:19:32 +0100

    kod ze seste hodiny

--- 06_OF_parser/src/OFParser/example.py
+++ 06_OF_parser/src/OFParser/example.py
@@ -2,4 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class dcExample():
-    pass
+    label: str
+    count: int
+
+    def __post_init__(self):
+        assert isinstance(self.label, str)
+        assert isinstance(self.count, int)
+
+    def prepare_data_for_plot(self, lst):
+        return lst + 1
--- 06_OF_parser/tests/test_example.py
+++ 06_OF_parser/tests/test_example.py
@@ -1,8 +1,35 @@
+import pytest
+
 from OFParser.example import dcExample
 
-def test_init():
-    a = dcExample
+# dataclass dcExample
+#   - label: str
+#   - count: int
 
-# def test_type_in_list_oflist_from_list() -> None:
-#     with pytest.raises(AssertionError):
-#         oflist = OfList.from_components_lists([['1', '2.3', '8']])
+class TestClass():
+    label = 'a'
+    count = 5
+
+    def test_init(self):
+        a = dcExample(label=self.label,count=self.count)
+
+    def test_repr(self):
+        a = dcExample(label=self.label,count=self.count)
+        assert str(a) == "dcExample(label='a', count=5)"
+
+def test_label_type() -> None:
+    with pytest.raises(AssertionError):
+        a = dcExample(label=1,count=5)
+
+def test_count_type() -> None:
+    with pytest.raises(AssertionError):
+        a = dcExample(label='a',count=5.0)
+
+def test_n_params() -> None:
+    with pytest.raises(TypeError):
+        a = dcExample('a',5,6)
+
+def test_prepared() -> None:
+    a = dcExample(label='a',count=5)
+    b = a.prepare_data_for_plot([1,2,3])
+    assert b == [2,3,4]
