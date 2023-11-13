bdf4b58ffbd50cdefcdae3aec86b1e7b8a38ad1a
tomasholusa <520871@mail.muni.cz>
2023-11-13 16:51:27 +0100

    Mistakes fixed, kinetic energy added

--- 06_OF_parser/tests/test_timestep_holusa.py
+++ 06_OF_parser/tests/test_timestep_holusa.py
@@ -1,45 +1,81 @@
 import pytest
 
-from OFParser.timestep.py import timestep
+from OFParser.TimeStep import TimeStep
 
 class TestLinearMomentumClass():
-    linear_momentum = (-1.44664513e-21 -1.94003635e-21 -5.42703062e-20)
+    Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20)
 
     def test_linear_momentum_exists(self) -> None:
-        a = timestep(linear_momentum = self.linear_momentum)
+        a = TimeStep(Linear_momentum = self.Linear_momentum, Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16,
+                      Absolute_linear_momentum = 5.43242364e-20)
 
     def test_linear_momentum_is_tuple() -> None:
         with pytest.raises(AssertionError):
-            a = timestep(linear_momentum = 5)
+            a = TimeStep(Linear_momentum = 5, Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16,
+                      Absolute_linear_momentum = 5.43242364e-20)
     
     def test_linear_momentum_three_values() -> None:
         with pytest.raises(AssertionError):
-            a = timestep(linear_momentum = (1, 4, 5, 6))
+            a = TimeStep(Linear_momentum = (1, 4, 5, 6), Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16,
+                      Absolute_linear_momentum = 5.43242364e-20)
         with pytest.raises(AssertionError):
-            a = timestep(linear_momentum = (1, 4))
+            a = TimeStep(Linear_momentum = (1, 4), Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16,
+                      Absolute_linear_momentum = 5.43242364e-20)
 
     def test_linear_momentum_tuple_of_floats() -> None:
         with pytest.raises(AssertionError):
-            a = timestep(linear_momentum = ("a", 5.0, {}))
+            a = TimeStep(Linear_momentum = ("a", 5.0, {}), Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16,
+                      Absolute_linear_momentum = 5.43242364e-20)
 
 class TestAbsoluteLinearMomentum():
-    absolute_linear_momentum = 5.43242364e-20
-    linear_momentum = (-1.44664513e-21 -1.94003635e-21 -5.42703062e-20)
+    Absolute_linear_momentum = 5.43242364e-20
+    Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20)
 
     def test_absolute_linear_momentum_exists(self) -> None:
-        a = timestep(absolute_linear_momentum = self.absolute_linear_momentum)
+        a = TimeStep(Absolute_linear_momentum = self.Absolute_linear_momentum, Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16, 
+                      Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20))
     
     def test_absolute_linear_momentum_is_float() -> None:
         with pytest.raises(AssertionError):
-            a = timestep(absolute_linear_momentum = "a")
+            a = TimeStep(Absolute_linear_momentum = "a", Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16, 
+                      Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20))
             
     def test_absolute_linear_momentum_gt_zero():
         with pytest.raises(AssertionError):
-            a = timestep(absolute_linear_momentum = -0.001)
+            a = TimeStep(Absolute_linear_momentum = -0.001, Time = 8.190e-05, Current_number_of_parcels = 6380,
+                      Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16, 
+                      Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20))
 
-    def test_absolute_linear_mumentum_eq_linear_momentum(self): #z nějakého důvodu nepasuje přesně
-        a = timestep(linear_momentum = self.linear_momentum, absolute_linear_momentum = self.absolute_linear_momentum)
-        sum(a.linear_momentum) == a.absolute_linear_momentum
+    def test_absolute_linear_mumentum_eq_linear_momentum(self):
+        a = TimeStep(Linear_momentum = self.Linear_momentum, Absolute_linear_momentum = self.Absolute_linear_momentum, Time = 8.190e-05, 
+                     Current_number_of_parcels = 6380, Current_mass_in_system = 4.23525746e-22, Linear_kinetic_energy = 2.41789442e-16)
+        for i in TimeStep.Linear_momentum:
+            c = c + i**2
+        c = c**0.5
+        assert c == a.Absolute_linear_momentum
+class TestLinearKineticEnergy():
+    Linear_kinetic_energy = 2.41789442e-16
 
+    def test_Linear_kinetic_energy_exists(self):
+        a = TimeStep(Linear_kinetic_energy = self.Linear_kinetic_energy, Absolute_linear_momentum = 5.43242364e-20, Time = 8.190e-05, 
+                     Current_number_of_parcels = 6380, Current_mass_in_system = 4.23525746e-22, 
+                     Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20))
+
+    def test_Linear_kinetic_energy_gt_zero():
+        a = TimeStep(Linear_kinetic_energy = -5.525, Absolute_linear_momentum = 5.43242364e-20, Time = 8.190e-05, 
+                     Current_number_of_parcels = 6380, Current_mass_in_system = 4.23525746e-22, 
+                     Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20))
+    
+    def test_Linear_kinetic_energy_float():
+        a = TimeStep(Linear_kinetic_energy = "a", Absolute_linear_momentum = 5.43242364e-20, Time = 8.190e-05, 
+                     Current_number_of_parcels = 6380, Current_mass_in_system = 4.23525746e-22, 
+                     Linear_momentum = (-1.44664513e-21, -1.94003635e-21, -5.42703062e-20))
     
 
