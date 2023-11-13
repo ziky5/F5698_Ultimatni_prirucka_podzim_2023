b07dbe72fa968d35298b82898b37d5e00cd235cc
madmad02 <mkoar@physics.muni.cz>
2023-11-13 16:11:46 +0100

    Test - time, mass

--- 06_OF_parser/tests/test_timestep_kolar.py
+++ 06_OF_parser/tests/test_timestep_kolar.py
@@ -5,32 +5,37 @@ from OFParser.timestep import TimeStep
 # Time counter tests
 
 def test_time_existence() -> None:
-    T = TimeStep(Time=1.0,Current_number_of_parcels=10,Current_mass_in_system=10.0,Linear_momentum=(5.0,4.0,3.0),Abs_Linear_momentum=1.0,Linear_kinetic_energy=x )
+    T = TimeStep(Time=1.0,Current_number_of_parcels=10,Current_mass_in_system=4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
     T.Time
 
 def test_time_type() -> None:
+    T = TimeStep(Time=1.0,Current_number_of_parcels=10,Current_mass_in_system=4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
     assert type(T.Time) == float
 
 def test_time_noneg() -> None:
-    time_noneg = float(abs(T.Time))
-    assert time_noneg == T.Time
+    with pytest.raises(AssertionError):
+        T = TimeStep(Time=-1.0,Current_number_of_parcels=10,Current_mass_in_system=4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
+
+def test_time_nonzero() -> None:
+    with pytest.raises(AssertionError):
+        T = TimeStep(Time=0.0,Current_number_of_parcels=10,Current_mass_in_system=4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
 
 # Particle mass tests
 
 def test_mass_existence() -> None:
+    T = TimeStep(Time=1.0,Current_number_of_parcels=10,Current_mass_in_system=4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
     T.Current_mass_in_system
 
 def test_mass_type() -> None:
+    T = TimeStep(Time=-1.0,Current_number_of_parcels=10,Current_mass_in_system=4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
     assert type(T.Current_mass_in_system) == float
 
 def test_mass_noneg() -> None:
-    mass_noneg = float(abs(T.Current_mass_in_system))
-    assert mass_noneg == T.Current_mass_in_system
+    with pytest.raises(AssertionError):
+        T = TimeStep(Time=1.0,Current_number_of_parcels=10,Current_mass_in_system=-4.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=-1.0)
 
+def test_mass_toenergy():
+    with pytest.raises(AssertionError):
+        T = TimeStep(Time=1.0,Current_number_of_parcels=10,Current_mass_in_system=5.5,Linear_momentum=(2.0,2.0,1.0),Abs_Linear_momentum=3.0,Linear_kinetic_energy=1.0)
 
-
-
-
-
-
-
+# %%
