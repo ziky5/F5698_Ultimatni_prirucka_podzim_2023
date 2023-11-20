35413bb25d897239f18ced2c8d329c33c738e19a
madmad02 <mkoar@physics.muni.cz>
2023-11-20 17:27:55 +0100

    Less than 0 bad timestep

--- 07_rekapitulace/tests/test_timestep.py
+++ 07_rekapitulace/tests/test_timestep.py
@@ -55,6 +55,17 @@ bad_timestep_data = [
         ),
         ValueError,
         "could not convert string to float: 'NO'",
+    ),
+    (
+        dict(
+            time=-1.0,
+            current_number_of_parcels=10,
+            current_mass_in_system=4.5,
+            linear_momentum=(2.0, 2.0, 1.0),
+            linear_kinetic_energy=1.0,
+        ),
+        AssertionError,
+        "value of 'time' can not be < 0",
     )
 ]
 
