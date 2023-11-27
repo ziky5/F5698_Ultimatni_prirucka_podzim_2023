285eed5fafe90e45677acdf0d7e6a4b3cbf8588b
Petr Zikán <zikan@plasmasolve.com>
2023-11-27 16:19:41 +0100

    test_from_str added

--- 07_rekapitulace/tests/test_timestep.py
+++ 07_rekapitulace/tests/test_timestep.py
@@ -325,3 +325,35 @@ def test_bad_timestep(timestep_bad_data):
         TimeStep(**ts_kwargs)
 
     assert e.value.args[0] == exc_msg
+
+
+DATA = """\
+Time = 8.190e-05
+
+Evolving kinematicCloud
+
+Solving 3-D cloud kinematicCloud
+Cloud: kinematicCloud
+    Current number of parcels       = 6380
+    Current mass in system          = 4.23525746e-22
+    Linear momentum                 = (-1.44664513e-21 -1.94003635e-21 -5.42703062e-20)
+   |Linear momentum|                = 5.43242364e-20
+    Linear kinetic energy           = 2.41789442e-16
+    model1:
+        number of parcels added     = 500000
+        mass introduced             = 3.31916729e-20
+    Parcel fate (number, mass)      : patch .*
+      - escape                      = 493620, 3.27681468e-20
+      - stick                       = 0, 0
+"""
+
+
+def test_from_str():
+    ts = TimeStep.from_str(DATA)
+    assert ts == TimeStep(
+        time=8.190e-05,
+        current_number_of_parcels=6380,
+        current_mass_in_system=4.23525746e-22,
+        linear_momentum=(-1.44664513e-21, -1.94003635e-21, -5.42703062e-20),
+        linear_kinetic_energy=2.41789442e-16,
+    )
